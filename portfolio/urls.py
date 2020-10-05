0240a0f5dbba6fc6b258e8c46dfac21e9bdb65c5
Bernhard Kämpf <bernhard.kaempf@gmail.com>
2020-10-05 06:25:37 +0200

    bootstrap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
